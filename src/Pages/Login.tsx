8a15d77a6c4dc69bda50d3a2b1328b92735fe230
Jin hyeon <kangjin627@gmail.com>
2023-11-08 20:02:58 +0900

    ADD: 현재 작업 커밋

---
+++ src/Pages/Login.tsx
@@ -0,0 +1,7 @@
+import { Txt } from "../Components/Text"
+
+export const LoginPage = () => {
+    return (
+        <Txt typography="DisplayLarge">로그인 페이지 입니다.</Txt>
+    )
+}
---
+++ src/Styles/Font.ts
@@ -0,0 +1,94 @@
+export const FontStyle = {
+    DisplayLarge: {
+        fontWeight: 700,
+        fontSize: '57px',
+        lineHeight: '64px',
+        letterSpacing: '-0.25%',
+      },
+      DisplayMedium: {
+        fontWeight: 700,
+        fontSize: '45px',
+        lineHeight: '52px',
+        letterSpacing: '0%',
+      },
+      DisplaySmall: {
+        fontWeight: 700,
+        fontSize: '36px',
+        lineHeight: '44px',
+        letterSpacing: '0%',
+      },
+      HeadlineLarge: {
+        fontWeight: 600,
+        fontSize: '32px',
+        lineHeight: '40px',
+        letterSpacing: '0%',
+      },
+      HeadlineMedium: {
+        fontWeight: 600,
+        fontSize: '28px',
+        lineHeight: '36px',
+        letterSpacing: '0%',
+      },
+      HeadlineSmall: {
+        fontWeight: 600,
+        fontSize: '24px',
+        lineHeight: '32px',
+        letterSpacing: '0%',
+      },
+      TitleLarge: {
+        fontWeight: 600,
+        fontSize: '22px',
+        lineHeight: '28px',
+        letterSpacing: '0%',
+      },
+      TitleMedium: {
+        fontWeight: 500,
+        fontSize: '16px',
+        lineHeight: '24px',
+        letterSpacing: '0.15%',
+      },
+      TitleSmall: {
+        fontWeight: 500,
+        fontSize: '14px',
+        lineHeight: '20px',
+        letterSpacing: '0.1%',
+      },
+      LabelLarge: {
+        fontWeight: 500,
+        fontSize: '14px',
+        lineHeight: '20px',
+        letterSpacing: '0.1%',
+      },
+      LabelMedium: {
+        fontWeight: 500,
+        fontSize: '12px',
+        lineHeight: '16px',
+        letterSpacing: '0.5%',
+      },
+      LabelSmall: {
+        fontWeight: 500,
+        fontSize: '11px',
+        lineHeight: '16px',
+        letterSpacing: '0.5%',
+      },
+      BodyLarge: {
+        fontWeight: 400,
+        fontSize: '16px',
+        lineHeight: '24px',
+        letterSpacing: '0.5%',
+      },
+      BodyMedium: {
+        fontWeight: 400,
+        fontSize: '14px',
+        lineHeight: '20px',
+        letterSpacing: '0.25%',
+      },
+      BodySmall: {
+        fontWeight: 400,
+        fontSize: '12px',
+        lineHeight: '16px',
+        letterSpacing: '0.4%',
+      },
+}
+
+export type fontsKeyOfType = keyof typeof FontStyle
