db30512d20a56948fdd089a01ec017b5aeabc1ae
Jin hyeon <kangjin627@gmail.com>
2023-12-04 19:15:14 +0900

    FIX: 라우팅 수정

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
     <Routes>
       <Route path='/' element={<LoginPage />} />
       <Route path='/signup' element={<SignupPage />} />
-      <Route path='/Main' element={<MainPage />} />
+      <Route path='/main' element={<MainPage />} />
     </Routes>
   );
 }
