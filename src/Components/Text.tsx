0515c973c516188d4b41c6aba21bf71d4c72fc9e
Jin hyeon <kangjin627@gmail.com>
2023-12-04 19:16:16 +0900

    ADD: onClick, cursor 추가

--- src/Components/Text.tsx
+++ src/Components/Text.tsx
@@ -5,12 +5,16 @@ interface TxtProps {
     typography: keyof typeof FontStyle;
     children: ReactNode;
     color?: string;
+    onClick?: () => void;
+    cursor?: boolean;
 }
 
 export const Txt = ({
     typography,
     color,
     children,
+    onClick,
+    cursor,
     ...props
 }: TxtProps) => {
     return (
@@ -18,7 +22,9 @@ export const Txt = ({
             style={{
                 color: color,
                 ...(FontStyle[typography]),
+                cursor: cursor ? "pointer" : "default",
             }}
+            onClick={onClick}
             {...props}>
             {children}
         </p>
